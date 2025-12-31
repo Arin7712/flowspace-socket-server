6997cd8254a28a666fd1e35d06d36f1849fbd6b7
Arin7712 <aringawande7712@gmail.com>
2025-12-31 22:40:37 +0530

    added

--- socket-server.js
+++ socket-server.js
@@ -7,7 +7,7 @@ app.use(express.json());
 
 const httpServer = createServer(app);
 const io = new Server(httpServer, {
-  cors: { origin: "http://localhost:3000" },
+  cors: { origin: "*" },
 });
 
 io.on("connection", (socket) => {
